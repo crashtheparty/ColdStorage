8d43893d70defb0e7532b155c29ffa5ada0138d6
crashtheparty <laytfire2@gmail.com>
2020-08-12 12:54:42 -0600

    Release 2.1.1

--- src/org/ctp/coldstorage/nms/AnvilGUINMS.java
+++ src/org/ctp/coldstorage/nms/AnvilGUINMS.java
@@ -10,29 +10,32 @@ import org.ctp.crashapi.utils.ChatUtils;
 
 public class AnvilGUINMS {
 	public static void createAnvil(Player player, InventoryData anvil, boolean choice) {
-		if(ColdStorage.getPlugin().getConfiguration().getAnvilEdits()) switch(CrashAPI.getPlugin().getBukkitVersion().getVersionNumber()) {
-		case 1:
-			AnvilGUI_v1_13_R1.createAnvil(player, anvil, choice);
-			break;
-		case 2:
-		case 3:
-			AnvilGUI_v1_13_R2.createAnvil(player, anvil, choice);
-			break;
-		case 4:
-		case 5:
-		case 6:
-		case 7:
-		case 8:
-			AnvilGUI_v1_14_R1.createAnvil(player, anvil, choice);
-			break;
-		case 9:
-		case 10:
-		case 11:
-			AnvilGUI_v1_15_R1.createAnvil(player, anvil, choice);
-			break;
-		case 12:
-			AnvilGUI_v1_16_R1.createAnvil(player, anvil, choice);
-			break;
+		if (ColdStorage.getPlugin().getConfiguration().getAnvilEdits()) switch (CrashAPI.getPlugin().getBukkitVersion().getVersionNumber()) {
+			case 1:
+				AnvilGUI_v1_13_R1.createAnvil(player, anvil, choice);
+				break;
+			case 2:
+			case 3:
+				AnvilGUI_v1_13_R2.createAnvil(player, anvil, choice);
+				break;
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+				AnvilGUI_v1_14_R1.createAnvil(player, anvil, choice);
+				break;
+			case 9:
+			case 10:
+			case 11:
+				AnvilGUI_v1_15_R1.createAnvil(player, anvil, choice);
+				break;
+			case 12:
+				AnvilGUI_v1_16_R1.createAnvil(player, anvil, choice);
+				break;
+			case 13:
+				AnvilGUI_v1_16_R2.createAnvil(player, anvil, choice);
+				break;
 		}
 		else {
 			anvil.close(false);
